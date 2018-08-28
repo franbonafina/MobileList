c88d0531dbf9931da07d7fba38924ce57b45679a
franb <franbonafina1@gmail.com>
2018-08-28 17:06:01 -0300

    Implement middleware

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,16 +1,21 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' 
 import rootReducer from '../reducers';
+import axios from 'axios';
+import axiosMiddleware from 'redux-axios-middleware';
+import thunkMiddleware from 'redux-thunk';
 
 const persistConfig = {
   key: 'root',
   storage,
 }
 
+const baseUrl = axios.create({baseUrl: 'https://jsonplaceholder.typicode.com/', responseType:'json'});
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-let store = createStore(persistedReducer);
+let store = createStore(persistedReducer, applyMiddleware(thunkMiddleware, axiosMiddleware(baseUrl)));
 let persistor = persistStore(store);
 
 export default {store, persistor};
