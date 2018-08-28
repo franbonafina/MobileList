3a23c65619ae4ff3557270c42c28b8fb1083e754
franb <franbonafina1@gmail.com>
2018-08-28 15:34:54 -0300

    Is intent to persist state of the app. Some changes with the configureStore class, and the app js.

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from './reducers';
+
+
 import { Header } from './components/common';
 import LibraryList from './components/LibraryList';
 
-import configureStore from './store/configureStore';  
+import { createStore } from 'redux';
+import { store, persistor } from './store/configureStore';  
 import { PersistGate } from 'redux-persist/integration/react'
-
-const store = configureStore();
 
 const App = () => {
   return (
     <Provider store={store}>
-    	<PersistGate loading={null} persistor={persistor}>
+    	<PersistGate loading={null} persistor={persistor} >
 	      <View style={{ flex: 1 }}>
 	        <Header headerText="Albums" />
 	        <LibraryList />
--- store/configureStore.js
+++ store/configureStore.js
@@ -1,8 +1,7 @@
 import { createStore } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
-
-import reducers from './reducers';
+import storage from 'redux-persist/lib/storage' 
+import reducers from '../reducers';
 
 const persistConfig = {
   key: 'root',
